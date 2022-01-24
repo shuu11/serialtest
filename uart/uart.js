a0bdb5e6cf0f0768c8d56f497c5d178d5c2a8cc8
shuu11 <shuukuubas@gmail.com>
2022-01-24 21:27:26 +0900

    serialtest

--- uart/uart.js
+++ uart/uart.js
@@ -1,8 +1,9 @@
-const fs = require('fs');
-const SerialPort = require('serialport');
-const Delimiter = require('@serialport/parser-delimiter');
-const ByteLength = require('@serialport/parser-byte-length');
-const InterByteTimeout = require('@serialport/parser-inter-byte-timeout');
+import fs from 'fs';
+import chalk from 'chalk';
+import SerialPort from 'serialport';
+import Delimiter from '@serialport/parser-delimiter';
+import ByteLength from '@serialport/parser-byte-length';
+import InterByteTimeout from '@serialport/parser-inter-byte-timeout';
 
 const port = new SerialPort('COM3', {
 	baudRate: 19200,
@@ -14,12 +15,6 @@ const port = new SerialPort('COM3', {
 const parser = port.pipe(new Delimiter({ delimiter: '\0' })); //	区切り文字の指定
 // const parser = port.pipe(new ByteLength({ length: 11 }));	//	受信文字列数の指定
 // const parser = port.pipe(new InterByteTimeout({ interval: 30 }));	//	受信時間のタイムアウトの指定
-
-const logColor = {
-	yellow: '\x1b[38;2;255;255;50m',
-	orange: '\x1b[38;2;255;140;0m',
-	reset: '\x1b[0m',
-};
 
 const srcPath = {
 	fs: './uart/log.txt',
@@ -42,7 +37,7 @@ port.on('open', () => {
 parser.on('data', (data) => {
 	const text = `Receive data:`;
 
-	console.log(`${text}${logColor.yellow}${data}${logColor.reset}`);
+	console.log(`${text}${chalk.yellow(data)}`);
 
 	fs.appendFile(srcPath.fs, `${text}${data}\n`, (err) => {
 		if (err) {
@@ -55,7 +50,7 @@ parser.on('data', (data) => {
 		const text = 'Send data:';
 		const data = 'Hi Tom';
 
-		console.log(`${text}${logColor.orange}${data}${logColor.reset}`);
+		console.log(`${text}${chalk.cyan(data)}`);
 
 		port.write(data, () => {
 			fs.appendFile(srcPath.fs, `${text}${data}\n`, (err) => {
